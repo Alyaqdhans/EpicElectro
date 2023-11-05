2100c19c19f8650271a1c86add437229ea82e6ef
Alyaqdhan <alyaqdhan690s@gmail.com>
2023-11-05 17:00:26 +0400

    login

--- Login/index.php
+++ Login/index.php
@@ -2,58 +2,7 @@
     <header>
         <title>Login Page</title>
         <link rel="stylesheet" href="../style.css">
-        <style>
-            body {
-                display: flex;
-                justify-content: center; /* horizontally */
-                align-items: center; /* vertically */
-            }
-
-            .main {
-                background: white;
-                width: fit-content;
-                height: fit-content;
-                padding: 1rem;
-                border-radius: var(--br);
-                box-shadow: 0 0 1rem black;
-            }
-
-            input {
-                display: block;
-                padding: .5rem;
-                margin: .5rem;
-            }
-
-            .buttons {
-                display: flex;
-                justify-content: center;
-                gap: 0.1rem;
-            }
-
-            .btn {
-                width: fit-content;
-                background: var(--clr-secondary);
-                padding: .5rem;
-                margin: 0;
-                border: 0;
-                border-radius: var(--br);
-                text-decoration: none;
-                font-weight: var(--fw-bold);
-                font-size: var(--fs-normal);
-            }
-
-            .btn:hover {
-                background: var(--clr-secondary-light);
-            }
-
-            .left {
-                border-radius: var(--br) 0 0 var(--br);
-            }
-
-            .right {
-                border-radius: 0 var(--br) var(--br) 0;
-            }
-        </style>
+        <link rel="stylesheet" href="style.css">
     </header>
     <body>
         <form action="process.php" method="post">
