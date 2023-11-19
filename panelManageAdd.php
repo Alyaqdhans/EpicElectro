dd2cad4c6d9a202f7ed0a409351ec19de014f49d
Alyaqdhan <alyaqdhan690s@gmail.com>
2023-11-19 20:50:23 +0400

    add page

--- panelManageAdd.php
+++ panelManageAdd.php
@@ -1,11 +1,12 @@
 <html>
     <head>
         <?php include('link.php') ?>
-        <title>EpicElectro | Add</title>
+        <title>EpicElectro | Supply</title>
     </head>
     <body>
         <?php
         include('header.php');
+        include('connect.php');
 
         if (!isset($_SESSION['TYPE'])) {
             header('Location: error.php?ec=1'); // login required
@@ -19,12 +20,63 @@
         ?>
         
         <div class="wrapper">
-            <div class="container">
+            <form class="container create" action="panelManageAddProcess.php" method="post">
                 <fieldset>
-                    <legend>Add Items</legend>
-                    
+                    <legend>Add Supply</legend>
+
+                    <?php
+                    $query = "select * from items where iCode = '{$_GET['ic']}'";
+                    $result = mysqli_query($conn, $query) or die("Error in query: <mark>$query</mark> <p>". mysqli_error($conn));
+                    $data = mysqli_fetch_assoc($result);
+
+                    echo "<input type='hidden' name='code' value='{$_GET['ic']}'>";
+                    echo "<input type='hidden' name='prevqty' value='{$data['iQty']}'>";
+                    ?>
+
+                    <label>
+                        Item Name:<br>
+                        <?php echo "<h3>[ {$data['iDesc']} ]</h3>"; ?>
+                    </label>
+
+                    <label>
+                        Item Supplier:<br>
+                        <?php
+                        echo "<select name='supplier' required>";
+                        echo "<option value=''> Suppliers </option>";
+
+                        $query = "select * from suppliers";
+                        $result = mysqli_query($conn, $query) or die("Error in query: <mark>$query</mark> <p>". mysqli_error($conn));
+                        
+                        while ($data2 = mysqli_fetch_assoc($result)) {
+                            if ($data2['sId'] == $data['iSupplierId']) {$s = "selected";}
+                            else {$s = "";}
+                            echo "<option value='{$data2['sId']}' $s> {$data2['sName']} </option>";
+                        }
+                        echo "</select>";
+                        ?>
+                    </label>
+
+                    <label>
+                        Item Quantity:<br>
+                        <?php echo "<input type='number' name='qty' value='{$data['iQty']}' required>"; ?>
+                    </label>
+
+                    <label>
+                        Item Cost: (Purchased for)<br>
+                        <?php echo "<input type='number' name='cost' value='{$data['iCost']}' required>"; ?>
+                    </label>
+
+                    <label>
+                        Item Price: (Sell for)<br>
+                        <?php echo "<input type='number' name='price' value='{$data['iPrice']}' required>"; ?>
+                    </label>
                 </fieldset>
-            </div>
+
+                <div class="buttons">
+                    <input class="btn left" type="submit" value="Save">
+                    <a class="btn right" href='panelManage.php'> Cancel </a>
+                </div>
+            </form>
         </div>
 
         <?php include('footer.php'); ?>
---
+++ panelManageAddProcess.php
@@ -0,0 +1,37 @@
+<?php
+include('library.php');
+include('connect.php');
+
+if (!isset($_POST['code'])) {
+    header('Location: error.php?ec=-1'); // entered page without button
+    exit;
+}
+
+$errors = [];
+
+if ($_POST['qty'] < 0) {$errors[] = "Quantity cannot be less than zero";}
+
+if ($_POST['cost'] < 1) {$errors[] = "Cost cannot be less than one";}
+
+if ($_POST['price'] < 1) {$errors[] = "Price cannot be less than one";}
+
+if (count($errors) == 0) {
+    $query = "update items set";
+    $query .= " iSupplierId = '{$_POST['supplier']}',";
+    $query .= " iQty = '{$_POST['qty']}',";
+    $query .= " iCost = '{$_POST['cost']}',";
+    $query .= " iPrice = '{$_POST['price']}'";
+    $query .= " where iCode = {$_POST['code']}";
+    mysqli_query($conn, $query) or die("Error in query: <mark>$query</mark> <p>". mysqli_error($conn));
+
+    if ($_POST['qty'] > $_POST['prevqty']) { // update date when added new supply
+        $date = date('Y-m-d');
+        $query = "update items set iLastPurchasedDate = '$date' where iCode = '{$_POST['code']}'";
+        mysqli_query($conn, $query) or die("Error in query: <mark>$query</mark> <p>". mysqli_error($conn));
+    }
+
+    header("Location: panelManage.php");
+} else {
+    DisplayErrors();
+}
+?>
