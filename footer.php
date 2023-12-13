623671c8a1ce07e6021c17d29076c7171fd9c0ce
Alyaqdhan <alyaqdhan690s@gmail.com>
2023-12-13 20:09:14 +0400

    footer update

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <footer id="down">
     <label>
-        &copy; Copyright of EpicElectro is Reserved to 
-        <abbr title="University of Technology and Applied Sciences">UTAS</abbr>
+        &copy; Copyright is reserved to 
+        <abbr title="s26s2025@nct.edu.om">Alyaqdhan</abbr>,
+        <abbr title="s26s1969@nct.edu.om">Hassan</abbr>,
+        <abbr title="s76s1937@nct.edu.om">Abdullrahman</abbr> &
+        <a href="https://www.utas.edu.om/" target="_blank" 
+        title="University of Technology and Applied Sciences of Nizwa">UTAS Nizwa</a>
     </label>
 
     <label>
